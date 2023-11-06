0861ecc8842b14864a42f7f044121fa73706c4c5
Xia Zhenchao <xiazhenchao96@gmail.com>
2023-11-05 21:45:41 -0500

    fix issue

---
+++ index.js
@@ -0,0 +1,45 @@
+// THESE ARE NODE APIs WE WISH TO USE
+const express = require('express')
+const cors = require('cors')
+const dotenv = require('dotenv')
+const cookieParser = require('cookie-parser')
+const path = require('path')
+var mongoose = require('mongoose');
+
+
+// CREATE OUR SERVER
+dotenv.config()
+const PORT = process.env.PORT || 4000;
+const app = express()
+
+// SETUP THE MIDDLEWARE
+app.use(express.urlencoded({ extended: true }))
+app.use(cors({
+    origin: ["http://localhost:3000"],
+    credentials: true
+}))
+app.use(express.json())
+app.use(cookieParser())
+
+// SETUP OUR OWN ROUTERS AS MIDDLEWARE
+const authRouter = require('./routes/auth-router')
+app.use('/auth', authRouter)
+
+//DB configs
+mongoose.connect(process.env.MONGO_URI).catch((err)=> console.log(err))
+
+// INITIALIZE OUR DATABASE OBJECT
+const db = require('./db')
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
+if(process.env.NODE_ENV==="production"){
+    app.use(express.static("client/build"));
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
+}
+
+// PUT THE SERVER IN LISTENING MODE
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+
+
