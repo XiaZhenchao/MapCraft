21e81c0de1ca44f8845f8a5caf78c156da5c52c8
Yonger <yongerfan12@gmail.com>
2023-11-06 20:59:43 -0500

    fix

--- cypress.config.js
+++ cypress.config.js
@@ -5,5 +5,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    supportFile: "test/cypress/support/e2e.js", 
+    specPattern: "test/cypress/e2e/**/*.cy.{js,jsx,ts,tsx}" 
   },
 });
