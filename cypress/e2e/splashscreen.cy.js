dc87e0370a864e94886ad1296301c8697ced3ffb
a1149934007@gmail.com <haoshui.gao@stonybrook.edu>
2023-11-08 02:24:46 -0500

    fix cypress test

--- cypress/e2e/splashscreen.cy.js
+++ cypress/e2e/splashscreen.cy.js
@@ -1,6 +1,6 @@
 describe('SplashScreen', () => {
   it('the SplashScreen contains the correct UI element', () => {
-    cy.visit('http://localhost:3000/')
+    cy.visit('https://mapcraft-55160ee4aae1.herokuapp.com/')
     cy.get('#splash-screen')
       .should("exist")
     cy.get('.MuiTypography-root > a')
