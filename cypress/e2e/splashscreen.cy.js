b1ef208a67c586e4fb15c04f462808bc687e7bed
Kuroemon7 <99367938+Kuroemon7@users.noreply.github.com>
2023-11-08 04:34:54 -0500

    Update splashscreen.cy.js

--- cypress/e2e/splashscreen.cy.js
+++ cypress/e2e/splashscreen.cy.js
@@ -4,13 +4,16 @@ Cypress.on("uncaught:exception", (err, runnable) => {
 
 describe('SplashScreen', () => {
   it('the SplashScreen contains the correct UI element', () => {
-    cy.wait(2000);
     cy.visit('https://mapcraft-55160ee4aae1.herokuapp.com/')
     cy.get('#splash-screen')
       .should("exist")
-    cy.wait(2000);
+  })
+})
+
+describe('.MuiTypography-root', () => {
+  it('check if the MUI typograph can be read or not ', () => {
+    cy.visit('https://mapcraft-55160ee4aae1.herokuapp.com/')
     cy.get('.MuiTypography-root > a')
       .should("exist")
-    cy.wait(2000);
   })
 })
