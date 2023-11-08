249ae7db3acd76d59341f7351f59c6f8e78a16c6
Kuroemon7 <99367938+Kuroemon7@users.noreply.github.com>
2023-11-08 03:57:04 -0500

    Update splashscreen.cy.js

--- cypress/e2e/splashscreen.cy.js
+++ cypress/e2e/splashscreen.cy.js
@@ -1,6 +1,7 @@
 describe('SplashScreen', () => {
   it('the SplashScreen contains the correct UI element', () => {
-   
+    cy.wait(2000);
+    cy.visit('https://mapcraft-55160ee4aae1.herokuapp.com/')
     cy.get('#splash-screen')
       .should("exist")
     cy.wait(2000);
