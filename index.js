b0e6bdbc3fc500ec913aac5b0bac27941a3aead4
a1149934007@gmail.com <haoshui.gao@stonybrook.edu>
2023-11-09 23:38:06 -0500

    change local to heroku

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const app = express()
 // SETUP THE MIDDLEWARE
 app.use(express.urlencoded({ extended: true }))
 app.use(cors({
-    origin: ["http://localhost:3000", "https://mapcraft-55160ee4aae1.herokuapp.com/"],
+    origin: ["https://mapcraft-55160ee4aae1.herokuapp.com/"],
     credentials: true
 }))
 app.use(express.json())
