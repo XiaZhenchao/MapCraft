8462aa1ef3f9dc7576c683b69e62d5ed6ede2f97
Yonger <yongerfan12@gmail.com>
2023-11-10 19:59:59 -0500

    fix

--- routes/auth-router.js
+++ routes/auth-router.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const AuthController = require('../controllers/auth-controller')
 
-router.post('/register', AuthController.registerUser)
+router.post('/auth/register', AuthController.registerUser)
 router.post('/login', AuthController.loginUser)
 router.get('/logout', AuthController.logoutUser)
 router.get('/loggedIn', AuthController.getLoggedIn)
