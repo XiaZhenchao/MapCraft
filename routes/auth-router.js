eeecea1d03a9ddc0d5c5ca9306a95d8b8f53a21b
Xia Zhenchao <xiazhenchao96@gmail.com>
2023-11-12 21:54:09 -0500

    solve cors error for debugging3

--- routes/auth-router.js
+++ routes/auth-router.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const AuthController = require('../controllers/auth-controller');
-const auth = require('../middleware/auth'); 
+const express = require('express')
+
+const router = express.Router()
 const AuthController = require('../controllers/auth-controller')
 
 router.post('/register', AuthController.registerUser)
