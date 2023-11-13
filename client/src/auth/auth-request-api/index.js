45db634ca7f30f482556ec6935c955662e43d366
Xia Zhenchao <xiazhenchao96@gmail.com>
2023-11-12 22:48:24 -0500

    Revert "solve internet issue"
    This reverts commit e053dd5806512adf5b29060d150b146e189261d7.

--- client/src/auth/auth-request-api/index.js
+++ client/src/auth/auth-request-api/index.js
@@ -13,7 +13,7 @@
 import axios from 'axios'
 axios.defaults.withCredentials = true;
 const baseURL = process.env.NODE_ENV === 'production'
-  ? 'https://mapcraft-55160ee4aae1.herokuapp.com/auth'
+  ? 'https://mapcraft-55160ee4aae1.herokuapp.com/api'
   : 'http://localhost:4000'; // Your local development server
 const api = axios.create({
     baseURL,
