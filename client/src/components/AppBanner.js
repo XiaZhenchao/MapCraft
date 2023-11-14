5a72190b7756ccc08cc56c0608b7a092323d5aab
Yonger <yongerfan12@gmail.com>
2023-11-14 18:15:11 -0500

    continue as guest

--- client/src/components/AppBanner.js
+++ client/src/components/AppBanner.js
@@ -58,6 +58,7 @@ export default function AppBanner() {
         >
             <MenuItem onClick={handleMenuClose}><Link to='/login/'>Login</Link></MenuItem>
             <MenuItem onClick={handleMenuClose}><Link to='/register/'>Create New Account</Link></MenuItem>
+            <MenuItem onClick={handleMenuClose}><Link to='/community/'>Continue As Guest</Link></MenuItem>
         </Menu>
     );
     const loggedInMenu = 
