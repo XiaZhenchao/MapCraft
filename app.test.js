fb24eb8a6551e5f1284f3ba43cd8f6801d2723b5
Xia Zhenchao <xiazhenchao96@gmail.com>
2023-11-22 16:43:37 -0500

    add jest and super test

--- app.test.js
+++ app.test.js
@@ -4,37 +4,73 @@ const express = require('express');
 const AuthController = require('./controllers/auth-controller.js');
 const { app, db} = require('./index');
 
-// beforeAll(async () => {
-//   // Increase the timeout for dropping the collection
-//   jest.setTimeout(10000);
-//   await db.collection('users').drop();
-// });
+beforeAll(async () => {
+    // Increase the timeout for dropping the collection
+    jest.setTimeout(10000);
+    await db.collection('users').deleteOne({ email: 'test43243123246@gmail.com' });
+  });
 
-// describe("Test application", () => {
-//   test('Test Register user', async () => {
-//     const res = await request(app)
-//       .post('/auth/register')
-//       .send({
-//         firstName: 'test41212336',
-//         lastName: 'test41212336',
-//         email: 'test43243123246@gmail.com',
-//         password: 'testtest46',
-//         passwordVerify: 'testtest46'
-//       });
-//     expect(res.statusCode).toBe(200);
-//   });
+describe("Test application", () => {
+  test('Test Register user', async () => {
+    const res = await request(app)
+      .post('/auth/register')
+      .send({
+        firstName: 'test41212336',
+        lastName: 'test41212336',
+        email: 'test43243123246@gmail.com',
+        password: 'testtest46',
+        passwordVerify: 'testtest46'
+      });
+    expect(res.statusCode).toBe(200);
+  });
  
-// });
+});
 
-// describe("Test application", () => {
-//     test('Test Login user', async () => {
-//         const res = await request(app)
-//           .post('/auth/login')
-//           .send({
-//             email: 'test43243123246@gmail.com',
-//             password: 'testtest46',
-//           });
-//         expect(res.statusCode).toBe(200);
-//       });
+describe("Test application", () => {
+    // ... Previous test cases for registration and login
+  
+    let authToken; // Define authToken variable outside the test cases
+  
+    test('Test Login user', async () => {
+      const res = await request(app)
+        .post('/auth/login')
+        .send({
+          email: 'test43243123246@gmail.com',
+          password: 'testtest46',
+        });
+      expect(res.statusCode).toBe(200);
+      authToken = res.body.token; // Assign the token to the authToken variable
+    });
+  
+    test('Add new map', async () => {
+      if (!authToken) {
+        return;
+      }
+  
+      const res = await request(app)
+        .post('/auth/map')
+        .set('Authorization', `Bearer ${authToken}`)
+        .send({
+          mapName: 'New Map',
+        });
+  
+      expect(res.statusCode).toBe(200);
+
+    });
+
+
+    test('Delete map', async () => {
+        // Ensure that a map ID is available for deletion
+        if (!authToken || !mapId) {
+          // Handle cases where either authToken or mapId is not available
+          return;
+        }
     
-//     });
+        const res = await request(app)
+          .delete(`/auth/map/${mapId}`) // Replace with the endpoint to delete a map
+          .set('Authorization', `Bearer ${authToken}`);
+    
+        expect(res.statusCode).toBe(200);
+        // Add assertions based on your application's response
+      });
+  });
